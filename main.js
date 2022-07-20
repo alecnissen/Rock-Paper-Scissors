77ec6f643aaff1bff3360b9d53a40f38a53436f2
alecnissen <alecnissen@yahoo.com>
2022-07-19 23:52:53 -0400

    if else conditional logic written, now I will try to use a switch statement in the conditional logic, along with making the parameters to accept case insensitive text

--- main.js
+++ main.js
@@ -9,4 +9,83 @@ function getComputerChoice () {
     return value;   
 } 
 
+// function playRound(playerSelection, computerSelection) { 
+//     switch (computerSelection) { 
+//         case 'paper'
 
+        // case value2:
+        // (playerSelection === 'scissors' && computerSelection === 'paper')     
+        // console.log("You win! Scissors beats paper"); 
+        // break; 
+
+        // case value3: 
+        // (playerSelection === 'rock' && computerSelection === 'scissors') 
+        // console.log("You win! Rock beats scissors");  
+        // break; 
+
+
+
+        function playRound(playerSelection, computerSelection) { 
+            if (playerSelection === 'paper' && computerSelection === 'rock') { 
+                return "You win! Paper beats rock"; 
+            } else if (playerSelection === 'scissors' && computerSelection === 'paper') { 
+                return "You win! Scissors beats paper"; 
+            } else if (playerSelection === 'rock' && computerSelection === 'scissors') { 
+                return "You win! Rock beats scissors"; 
+            } else if (playerSelection === 'rock' && computerSelection === 'paper') { 
+                return "You lose! Paper beats rock"; 
+            } else if (playerSelection === 'paper' && computerSelection === 'scissors') { 
+                return "You lose! Scissors beats paper"; 
+            } else if (playerSelection === 'scissors' && computerSelection === 'rock') { 
+                return "You lose! Rock beats scissors"; 
+            } else if (playerSelection === 'paper' && computerSelection === 'paper') { 
+                return "it's a tie!"; 
+            } else if (playerSelection === 'rock' && computerSelection === 'rock') { 
+                return "it's a tie!"; 
+            } else if (playerSelection === 'scissors' && computerSelection === 'scissors') { 
+                return "it's a tie!"; 
+            } else { 
+                return "error, try again"; 
+            }
+        } 
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function playRound(playerSelection, computerSelection) { 
+//     if (playerSelection === 'paper' && computerSelection === 'rock') { 
+//         return "You win! Paper beats rock"; 
+//     } else if (playerSelection === 'scissors' && computerSelection === 'paper') { 
+//         return "You win! Scissors beats paper"; 
+//     } else if (playerSelection === 'rock' && computerSelection === 'scissors') { 
+//         return "You win! Rock beats scissors"; 
+//     } else if (playerSelection === 'rock' && computerSelection === 'paper') { 
+//         return "You lose! Paper beats rock"; 
+//     } else if (playerSelection === 'paper' && computerSelection === 'scissors') { 
+//         return "You lose! Scissors beats paper"; 
+//     } else if (playerSelection === 'scissors' && computerSelection === 'rock') { 
+//         return "You lose! Rock beats scissors"; 
+//     } else if (playerSelection === 'paper' && computerSelection === 'paper') { 
+//         return "it's a tie!"; 
+//     } else if (playerSelection === 'rock' && computerSelection === 'rock') { 
+//         return "it's a tie!"; 
+//     } else if (playerSelection === 'scissors' && computerSelection === 'scissors') { 
+//         return "it's a tie!"; 
+//     } else { 
+//         return "error, try again"; 
+//     }
+// } 
