c072219f30b2b5d33996ad1c73223f950356d3e7
alecnissen <alecnissen@yahoo.com>
2022-07-22 23:38:31 -0400

    removed score incrementors inside playRound function, will capture playRound result and pass it into game function, working on game function

--- main.js
+++ main.js
@@ -1,30 +1,48 @@
 // getting random elements from the array of choices
 
+// mod
+
 let valuesArray = ['rock', 'paper', 'scissors']; 
 
 
 
 function getComputerChoice () { 
-    let value = valuesArray[Math.floor(Math.random() * valuesArray.length)]; 
-    return value;   
-} 
+    let value = valuesArray[Math.floor(Math.random() * valuesArray.length)];  
+    // let playerInput = prompt('Enter rock, paper, or scissors'); 
+    // console.log(value, playerInput); 
+    // playRound(playerInput, value);
+  // return statement 
+  return value; 
+}  
 
+
+
+    let playerScore = 0; 
+    let compScore = 0; 
 
 function playRound(playerSelection, computerSelection) { 
+
     if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock') { 
-        return "You win! Paper beats rock"; 
+        let result1 = "You win! Paper beats rock"; 
+        return result1;  
     } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper') { 
-        return "You win! Scissors beats paper"; 
+        let result2 = "You win! Scissors beats paper"; 
+        return result2; 
     } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') { 
-        return "You win! Rock beats scissors"; 
+        let result3 = "You win! Rock beats scissors"; 
+        return result3; 
     } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper') { 
-        return "You lose! Paper beats rock"; 
+        let result4 = "You lose! Paper beats rock"; 
+        return result4; 
     } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors') { 
-        return "You lose! Scissors beats paper"; 
+        let result5 = "You lose! Scissors beats paper"; 
+        return result5; 
     } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock') { 
-        return "You lose! Rock beats scissors"; 
+        let result6 = "You lose! Rock beats scissors"; 
+        return result6; 
     } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'paper') { 
-        return "it's a tie!"; 
+        let result7 = "it's a tie!"; 
+        return result7; 
     } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'rock') { 
         return "it's a tie!"; 
     } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'scissors') { 
@@ -32,12 +50,206 @@ function playRound(playerSelection, computerSelection) {
     } 
 }    
 
+// getting result of playround 
+let playRoundResult = playRound; 
 
-
-
-const playerSelection = "rock";
+// const playerSelection = "rock";
 let computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));        
+// console.log(playRound(playerSelection, computerSelection));    
+
+
+
+// pass result of playround to game function 
+// playround will need to be invoked 5 times 
+// the results will need to be printed and a winner must be logged at the end 
+
+// keep score inside of game function 
+function game() { 
+    let playerInput = prompt('Enter rock, paper, or scissors'); 
+
+        let result = playRound(playerInput, computerSelection);      
+   
+        for (let i = 0; result < 5; i++) {  
+          // let playRoundResult1 = playRound(playerInput, getComputerChoice);    
+          // calling playRound within the loop 
+          // it needs to be invoked 5 times... 
+         
+            if (playerScore > compScore) {                
+                // console.log(result); 
+                console.log('player won that round!'); 
+            } else {                 
+                // console.log(result); 
+                console.log('computer won that round!');
+            }
+        }
+            if (playerScore > compScore) {       
+            console.log('the player won!');   
+        }   else if (compScore > playerScore) { 
+            console.log('computer wins!'); 
+        }   else { 
+            console.log('It/s a tie!');      
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // getting random elements from the array of choices
+
+// let valuesArray = ['rock', 'paper', 'scissors']; 
+
+
+
+// function getComputerChoice () { 
+//     let value = valuesArray[Math.floor(Math.random() * valuesArray.length)]; 
+//     return value;   
+// } 
+
+// let playerScore = 0; 
+// let compScore = 0; 
+
+// function playRound(playerSelection, computerSelection) { 
+
+//     if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock') { 
+//         playerScore++ 
+//         return "You win! Paper beats rock"; 
+//     } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper') { 
+//         playerScore++ 
+//         return "You win! Scissors beats paper"; 
+//     } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') { 
+//         playerScore++ 
+//         return "You win! Rock beats scissors"; 
+//     } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper') { 
+//         compScore++
+//         return "You lose! Paper beats rock"; 
+//     } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors') { 
+//         compScore++
+//         return "You lose! Scissors beats paper"; 
+//     } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock') { 
+//         compScore++
+//         return "You lose! Rock beats scissors"; 
+//     } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'paper') { 
+//         return "it's a tie!"; 
+//     } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'rock') { 
+//         return "it's a tie!"; 
+//     } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'scissors') { 
+//         return "it's a tie!"; 
+//     } 
+// }    
+
+
+
+
+// // const playerSelection = "rock";
+// // let computerSelection = getComputerChoice();
+// // console.log(playRound(playerSelection, computerSelection));    
+
+
+
+
+
+// // Write a NEW function called game(). 
+// // Call the playRound function inside of this one to play a 5 round game that 
+// // keeps score and reports a winner or loser at the end.
+// // Remember loops? This is a great opportunity to use one to play those five rounds:
+
+// // for (let i = 0; i < 5; i++) {
+// //    // your code here!
+// // }
+// // At this point you should be using console.log() to display the results of each round 
+// // and the winner at the end.
+// // Use prompt() to get input from the user. Read the docs here if you need to.
+
+// // function 
+// // use prompt to have user enter their choice of RPS 
+// // save the result of the prompt to variable and pass it into the playround function 
+// // loop to keep track of the round, starts at 0 then ends at 5 
+// // keeps score - use score variable for both players 
+// // use a conditional to determine who won each round (increment++) and then console.log the results after each round, 
+// // console.log the winner of the round, then console.log the winner of the game 
+// // 
+
+// // How to determine a winner at the end of each round, what will I be comparing to determine the winner? The playRound function already determines a winner for a single round 
+// // can I use the playRound function as a helper function to get the results ? 
+
+
+// function game() { 
+//     let playerInput = prompt('Enter rock, paper, or scissors');
+
+//      let result = playRound(playerInput, getComputerChoice) 
+//         for (let i = 0; i < 5; i++) {  
+//             if (playerScore > compScore) {                
+//                 console.log('player won that round!');          
+//             } else {                 
+//                 console.log('computer won that round!');
+//             }
+//         }
+//             if (playerScore > computerScore) {       
+//             console.log('the player won!');   
+//         }   else if (computerScore > playerScore) { 
+//             console.log('computer wins!'); 
+//         }   else { 
+//             console.log('It/s a tie!');      
+//         } 
+    
+//     } 
+// console.log(game()); 
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -67,31 +279,42 @@ console.log(playRound(playerSelection, computerSelection));
 // can I use the playRound function as a helper function to get the results ? 
 
 
-function game() { 
-    let playerInput = prompt('Enter rock, paper, or scissors');     // getting the user input 
-    let playerScore = 0;   // setting a score variable for both, to keep track for each round, and the variable will be updated each round 
-    let computerScore = 0; 
+// function game() { 
+//     let playerInput = prompt('Enter rock, paper, or scissors');     // getting the user input 
+//     let playerScore = 0;   // setting a score variable for both, to keep track for each round, and the variable will be updated each round 
+//     let computerScore = 0; 
 
-    function playRound(playerInput, getComputerChoice) {   // calling the playRound func inside the game func, plugging in the values for both 
-        for (let i = 0; i < 5; i++) {   // loop to control how many round get played 
-            if (playerInput) {                 // conditional to check who won that round
-                console.log('player won that round!'); // log the results 
-                playerScore++         // update the score 
-            } else {                 // if the above fails, it will check this block 
-                console.log('computer won that round!');
-                computerScore++
-            }
-        }
-        if (playerScore > computerScore) {      // return the final results, outside of the loop, once the loop has finished 
-            console.log('the player won!');   // log the results for either using if else 
-        } else if (computerScore > playerScore) { 
-            console.log('computer wins!'); 
-        } else { 
-            console.log('It/s a tie!');      // account for in case of tie 
-        }
+//     function playRound(playerInput, getComputerChoice) {   // calling the playRound func inside the game func, plugging in the values for both 
+//         for (let i = 0; i < 5; i++) {   // loop to control how many round get played 
+//             if (playerInput) {                 // conditional to check who won that round
+//                 console.log('player won that round!'); // log the results 
+//                 playerScore++         // update the score 
+//             } else {                 // if the above fails, it will check this block 
+//                 console.log('computer won that round!');
+//                 computerScore++
+//             }
+//         }
+//         if (playerScore > computerScore) {      // return the final results, outside of the loop, once the loop has finished 
+//             console.log('the player won!');   // log the results for either using if else 
+//         } else if (computerScore > playerScore) { 
+//             console.log('computer wins!'); 
+//         } else { 
+//             console.log('It/s a tie!');      // account for in case of tie 
+//         }
     
-    }
-}
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
